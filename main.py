ec47f39e69a4b6814d37420cef697a88bd8799a3
Enzo G. Almeida <myenzo121@gmail.com>
2023-06-12 20:20:22 -0300

    first Commit

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ contato = {
 
 # Adicionando novo elemento
 contato['Pedro'] = "+555498888888"
+#alterar numero
+contato['Enzo'] = "55549484000"
+#excluir contatos
+
+print(contato.pop('Marias', "Contato nao encontrado!"))
+
+print(contato)
